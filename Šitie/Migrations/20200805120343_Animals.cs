b9802fad8fe42538a9ff79bd303f27f4c7f4b9d6
Rokas Krankalis <rokas.krankalis@ktu.edu>
2020-08-05 15:06:30 +0300

    Add animals database migration

---
+++ Šitie/Migrations/20200805120343_Animals.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Šitie.Migrations
+{
+    public partial class Animals : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "Id", "ArivalDate", "ArrivedFrom", "BornDate", "ChipDate", "DepartureDate", "Description", "FurColor", "FurType", "Gender", "HealthCondition", "OrganizationData", "Status", "Type", "VaccineDate" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2020, 7, 26, 15, 3, 42, 695, DateTimeKind.Local).AddTicks(7901), "Kaunas", new DateTime(2001, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2020, 6, 16, 15, 3, 42, 697, DateTimeKind.Local).AddTicks(9994), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Labai jautrus", "Rudas su juodais taškeliais", 1, 0, "Sveikas, trūksta 1 kojos", @"Katino svajonė 
+                 Partizanų g. 15, Kaunas 50203 
+                 (8-655) 15024", 0, 1, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, new DateTime(2020, 4, 27, 15, 3, 42, 698, DateTimeKind.Local).AddTicks(7911), "Vilnius", new DateTime(2011, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2020, 7, 26, 15, 3, 42, 698, DateTimeKind.Local).AddTicks(7941), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Labai miela, reikalauja dėmesio", "Juoda baltomis letenėlėmis", 2, 1, "Sveika, alergiška šunims", @"Katino svajonė 
+                 Partizanų g. 15, Kaunas 50203 
+                 (8-655) 15024", 0, 0, new DateTime(2015, 8, 5, 15, 3, 42, 698, DateTimeKind.Local).AddTicks(7951) },
+                    { 3, new DateTime(2020, 6, 16, 15, 3, 42, 698, DateTimeKind.Local).AddTicks(8089), "Klaipėda", new DateTime(2011, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Driežas", "Žalsvai pilkas", 1, 0, "Sveikas", @"Katino svajonė 
+                 Partizanų g. 15, Kaunas 50203 
+                 (8-655) 15024", 0, 2, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 4, new DateTime(2020, 4, 27, 15, 3, 42, 698, DateTimeKind.Local).AddTicks(8096), "Kaunas", new DateTime(2001, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2020, 6, 16, 15, 3, 42, 698, DateTimeKind.Local).AddTicks(8099), new DateTime(2020, 8, 4, 15, 3, 42, 698, DateTimeKind.Local).AddTicks(8104), "-", "Ruda", 0, 0, "Labai ligotas", @"Katino svajonė 
+                 Partizanų g. 15, Kaunas 50203 
+                 (8-655) 15024", 1, 0, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 5, new DateTime(2020, 4, 27, 15, 3, 42, 698, DateTimeKind.Local).AddTicks(8107), "Kaunas", new DateTime(2001, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2020, 6, 16, 15, 3, 42, 698, DateTimeKind.Local).AddTicks(8110), new DateTime(2020, 8, 3, 15, 3, 42, 698, DateTimeKind.Local).AddTicks(8113), "-", "Ruda", 0, 0, "Truputi ligotas", @"Katino svajonė 
+                 Partizanų g. 15, Kaunas 50203 
+                 (8-655) 15024", 2, 0, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 6, new DateTime(2020, 4, 27, 15, 3, 42, 698, DateTimeKind.Local).AddTicks(8117), "Kaunas", new DateTime(2001, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2020, 6, 16, 15, 3, 42, 698, DateTimeKind.Local).AddTicks(8120), new DateTime(2020, 7, 26, 15, 3, 42, 698, DateTimeKind.Local).AddTicks(8124), "-", "Ruda", 0, 1, "Puiki", @"Katino svajonė 
+                 Partizanų g. 15, Kaunas 50203 
+                 (8-655) 15024", 2, 0, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 6);
+        }
+    }
+}
--- Šitie/Models/AppDbContext.cs
+++ Šitie/Models/AppDbContext.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
-
+using System;
 namespace Šitie.Models
 {
     public class AppDbContext : DbContext
@@ -20,6 +20,46 @@ namespace Šitie.Models
                     Email = "domas@gmail.com", Password = "asdfasfsafas", IsAdmin = false},
                 new User { Id = 3, Name = "Lukas", Surname = "Lukaitis", 
                     Email = "lukas.lukaitis@gmail.com", Password = "12345678", IsAdmin = false}
+                );
+            modelBuilder.Entity<Animal>().HasData(
+                new Animal { Id = 1, ArivalDate = DateTime.Now.AddDays(-10), ChipDate = DateTime.Now.AddDays(-50),
+                    VaccineDate = DateTime.MinValue, ArrivedFrom = "Kaunas", Type = AnimalType.Dog, 
+                    Gender = Gender.Male, BornDate = new DateTime(2001, 1, 1), FurType = FurType.Roughhair,
+                    FurColor = "Rudas su juodais taškeliais", Description = "Labai jautrus", HealthCondition = "Sveikas, trūksta 1 kojos",
+                    OrganizationData = "Katino svajonė \n Partizanų g. 15, Kaunas 50203 \n (8-655) 15024",
+                    Status = AnimalStatus.Shelter, DepartureDate = DateTime.MinValue},
+                new Animal { Id = 2, ArivalDate = DateTime.Now.AddDays(-100), ChipDate = DateTime.Now.AddDays(-10),
+                    VaccineDate = DateTime.Now.AddYears(-5), ArrivedFrom = "Vilnius", Type = AnimalType.Cat, 
+                    Gender = Gender.Female, BornDate = new DateTime(2011, 1, 1), FurType = FurType.Mediumhair,
+                    FurColor = "Juoda baltomis letenėlėmis", Description = "Labai miela, reikalauja dėmesio", 
+                    HealthCondition = "Sveika, alergiška šunims",
+                    OrganizationData = "Katino svajonė \n Partizanų g. 15, Kaunas 50203 \n (8-655) 15024",
+                    Status = AnimalStatus.Shelter, DepartureDate = DateTime.MinValue},
+                new Animal { Id = 3, ArivalDate = DateTime.Now.AddDays(-50), ChipDate = DateTime.MinValue,
+                    VaccineDate = DateTime.MinValue, ArrivedFrom = "Klaipėda", Type = AnimalType.Other, 
+                    Gender = Gender.Male, BornDate = new DateTime(2011, 1, 1), FurType = FurType.Roughhair,
+                    FurColor = "Žalsvai pilkas", Description = "Driežas", 
+                    HealthCondition = "Sveikas",
+                    OrganizationData = "Katino svajonė \n Partizanų g. 15, Kaunas 50203 \n (8-655) 15024",
+                    Status = AnimalStatus.Shelter, DepartureDate = DateTime.MinValue},
+                new Animal { Id = 4, ArivalDate = DateTime.Now.AddDays(-100), ChipDate = DateTime.Now.AddDays(-50),
+                    VaccineDate = DateTime.MinValue, ArrivedFrom = "Kaunas", Type = AnimalType.Cat, 
+                    Gender = Gender.Male, BornDate = new DateTime(2001, 1, 1), FurType = FurType.Shorthair,
+                    FurColor = "Ruda", Description = "-", HealthCondition = "Labai ligotas",
+                    OrganizationData = "Katino svajonė \n Partizanų g. 15, Kaunas 50203 \n (8-655) 15024",
+                    Status = AnimalStatus.Dead, DepartureDate = DateTime.Now.AddDays(-1)},
+                new Animal { Id = 5, ArivalDate = DateTime.Now.AddDays(-100), ChipDate = DateTime.Now.AddDays(-50),
+                    VaccineDate = DateTime.MinValue, ArrivedFrom = "Kaunas", Type = AnimalType.Cat, 
+                    Gender = Gender.Male, BornDate = new DateTime(2001, 1, 1), FurType = FurType.Shorthair,
+                    FurColor = "Ruda", Description = "-", HealthCondition = "Truputi ligotas",
+                    OrganizationData = "Katino svajonė \n Partizanų g. 15, Kaunas 50203 \n (8-655) 15024",
+                    Status = AnimalStatus.Adopted, DepartureDate = DateTime.Now.AddDays(-2)},
+                new Animal { Id = 6, ArivalDate = DateTime.Now.AddDays(-100), ChipDate = DateTime.Now.AddDays(-50),
+                    VaccineDate = DateTime.MinValue, ArrivedFrom = "Kaunas", Type = AnimalType.Cat, 
+                    Gender = Gender.Female, BornDate = new DateTime(2001, 1, 1), FurType = FurType.Shorthair,
+                    FurColor = "Ruda", Description = "-", HealthCondition = "Puiki",
+                    OrganizationData = "Katino svajonė \n Partizanų g. 15, Kaunas 50203 \n (8-655) 15024",
+                    Status = AnimalStatus.Adopted, DepartureDate = DateTime.Now.AddDays(-10)}
                 );
         }
     }
