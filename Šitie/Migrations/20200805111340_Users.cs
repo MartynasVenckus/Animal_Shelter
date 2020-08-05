ae9e6af4c486219785370f387cd923801d9684cf
Rokas Krankalis <rokas.krankalis@ktu.edu>
2020-08-05 14:15:59 +0300

    Add user database migration

---
+++ Šitie/Migrations/20200805111340_Users.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Šitie.Migrations
+{
+    public partial class Users : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "IsAdmin", "Name", "Password", "Surname" },
+                values: new object[] { 1, "vardenis.pvardenis@gmail.com", true, "Vardenis", "123456", "Pavardenis" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "IsAdmin", "Name", "Password", "Surname" },
+                values: new object[] { 2, "domas@gmail.com", false, "Domas", "asdfasfsafas", "Domaitis" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "IsAdmin", "Name", "Password", "Surname" },
+                values: new object[] { 3, "lukas.lukaitis@gmail.com", false, "Lukas", "12345678", "Lukaitis" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
--- Šitie/Models/AppDbContext.cs
+++ Šitie/Models/AppDbContext.cs
@@ -10,5 +10,17 @@ namespace Šitie.Models
 
         public virtual DbSet<Animal> Animals { get; set; }
         public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+                new User { Id = 1, Name = "Vardenis", Surname = "Pavardenis", 
+                    Email = "vardenis.pvardenis@gmail.com", Password = "123456", IsAdmin = true},
+                new User { Id = 2, Name = "Domas", Surname = "Domaitis", 
+                    Email = "domas@gmail.com", Password = "asdfasfsafas", IsAdmin = false},
+                new User { Id = 3, Name = "Lukas", Surname = "Lukaitis", 
+                    Email = "lukas.lukaitis@gmail.com", Password = "12345678", IsAdmin = false}
+                );
+        }
     }
 }
